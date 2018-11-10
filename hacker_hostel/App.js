4336d7c80363695713e0dce0990cb5c10e44104c
Dima Kurbatov <dskurbatov13@gmail.com>
2018-11-10 14:03:11 -0800

    remove meal generators from app to list component

--- hacker_hostel/App.js
+++ hacker_hostel/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Bookings from './components/Bookings';
-import Meals from './components/Meals';
+import ListOfMeals from './components/List_of_meals';
 import Error from './components/Error';
 import './index.css'
 
@@ -15,46 +15,6 @@ function isValidDateAndRange(str1, str2){
 	}
 	return false
 }
-
-function formateDate(year, month, date){
-	const now = new Date(year, month, date)
-	const nowMonth = now.getMonth()
-	const nowDate = now.getDate()
-	return `${now.getFullYear()}-${nowMonth < 10 ? '0' + nowMonth : nowMonth}-${nowDate < 10 ? '0' + nowDate : nowDate}`
-}
-
-// function assertArray(arr1, arr2){
-// 	const isSameLength = (arr1.length === arr2.length)
-// 	const isSame = arr1.every((item, idx) => {
-// 		return item === arr2[idx]
-// 	})
-// 	if(isSameLength && isSame){
-// 		return true
-// 	} else {
-// 		return false
-// 	}
-// }
-
-function generateRange(from, to){
-	const range = []
-	const dateFrom = new Date(...from.split('-'))
-	const fromArray = [dateFrom.getFullYear(), dateFrom.getMonth(), dateFrom.getDate()]
-	let current = from
-	while(current <= to){
-		range.push(current)
-		fromArray[2]++
-		current = formateDate(...fromArray)
-	}
-	return range
-}
-
-function generateMeals(name, from, to){
-	const range = generateRange(from, to)
-	return range.map((date, idx) => {
-		return <Meals key={name+idx} name={name} date={date}></Meals>
-	})
-}
-
 
 class App extends Component {
 	constructor(props){
@@ -95,9 +55,7 @@ class App extends Component {
 				{
 					this.state.hackers.filter(({ date }) => {
 						return date.length > 0 && isValidDateAndRange(date[0], date[1])
-					}).reduce((acc, { name, date }) => {
-						return acc.concat(generateMeals(name, date[0], date[1]))
-					}, [])
+					}).map(({name, date}) => <ListOfMeals name={name} date={date}></ListOfMeals>)
 				}
 			</div>
 		</div>);
