f245920d272ff22efef5179eeed3ce4550b628b8
Dima Kurbatov <dskurbatov13@gmail.com>
2018-11-10 13:15:58 -0800

    added date to meal component

--- hacker_hostel/components/Meals.js
+++ hacker_hostel/components/Meals.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
-const Meals = (({ name }) => {
+const Meals = (({ name, date }) => {
 	return (
 		<div className="col-xs-12  col-sm-12 col-md-12 col-lg-12">
 			<ol id="list">
 				<div>
-					<li className="morning">{`Breakfast for ${name}`}</li>
-					<li className="afternoon">{`Lunch for ${name}`}</li>
-					<li className="night">{`Dinner for ${name}`}</li>
+					<li className="morning">{`Breakfast for ${name} on ${date}`}</li>
+					<li className="afternoon">{`Lunch for ${name} on ${date}`}</li>
+					<li className="night">{`Dinner for ${name} on ${date}`}</li>
 				</div>
 			</ol>
 		</div>);
