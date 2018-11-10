2acd15495aabe79cb2303fdee2d4f61af1cf7e57
Dima Kurbatov <dskurbatov13@gmail.com>
2018-11-09 16:49:46 -0800

    updated component state and some methods

--- hacker_hostel/App.js
+++ hacker_hostel/App.js
@@ -5,49 +5,41 @@ import Error from './components/Error';
 
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            dates: [],
-            names: []
-        }
-        
-    }
-    
-    handleClick(dates, names){
-        console.log(dates, names)
-        this.setState({
-            dates,
-            names
-        })
-    }
-    
-    validate(){
-        const { dates } =  this.state
-        let start = null, end = null
-        for(let i = 0; i < dates.length; i++){
-            [start, end] = dates[i].split(' to ')
-            if(new Date(end) - new Date(start) < 0){
-                dates[i] = null
-            }
-        }
-        return this.setState({
-            dates: [...dates]
-        })
-    }
+	constructor(props){
+		super(props)
+		this.state = {
+				hackers: []
+		}	
+		
+		this.handleClick = this.handleClick.bind(this)
+	}
 
-    render() {
-        return (<div className="container-fluid">
-            <center>
-                <h2>Hacker Hostel</h2>
-            </center>
-            <div className="container">
-                <Bookings onClick={this.handleClick}></Bookings>
-                <Error></Error>
-                <Meals></Meals>
-            </div>
-        </div>);
-    }
+	handleClick(names, dates){
+		const len = names.length
+		const hackers = Array(len).fill(null)
+		for(let i = 0; i < len; i++){
+			hackers.push({
+				name: names[i],
+				date: dates[i]
+			})
+		}
+		return this.setState({
+			hackers
+		})
+	}
+	
+	render() {
+		return (<div className="container-fluid">
+			<center>
+				<h2>Hacker Hostel</h2>
+			</center>
+			<div className="container">
+				<Bookings handleClick={this.handleClick}></Bookings>
+				<Error></Error>
+				<Meals></Meals>
+			</div>
+		</div>);
+	}
 }
 
 export default App;
