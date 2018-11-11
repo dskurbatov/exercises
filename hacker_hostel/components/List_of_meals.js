770e4bf1e1cfb40ce41327699228ece737f24101
Dima Kurbatov <dskurbatov13@gmail.com>
2018-11-10 19:25:01 -0800

    replace few functions with single date iterator

--- hacker_hostel/components/List_of_meals.js
+++ hacker_hostel/components/List_of_meals.js
@@ -1,32 +1,18 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 import Meals from './Meals'
-
-function formateDate(year, month, date){
-	const now = new Date(year, month, date)
-	const nowMonth = now.getMonth()
-	const nowDate = now.getDate()
-	return `${now.getFullYear()}-${nowMonth < 10 ? '0' + nowMonth : nowMonth}-${nowDate < 10 ? '0' + nowDate : nowDate}`
-}
-
-function generateRange(from, to){
-	const range = []
-	const dateFrom = new Date(...from.split('-'))
-	const fromArray = [dateFrom.getFullYear(), dateFrom.getMonth(), dateFrom.getDate()]
-	let current = from
-	while(current <= to){
-		range.push(current)
-		fromArray[2]++
-		current = formateDate(...fromArray)
-	}
-	return range
-}
+import DateIterator from '../helpers/date_iterator'
 
 function generateMeals(name, from, to){
-	const range = generateRange(from, to)
-	return range.map((date, idx) => {
-		return <Meals key={name+idx} name={name} date={date}></Meals>
-	})
+	const meals = []
+	const iterator = new DateIterator(from, to)
+	let date = null, i = 0
+	while(iterator.isNext()){
+		date = iterator.next()
+		meals.push(<Meals key={name + i} name={name} date={date}></Meals>)
+		i++
+	}
+	return meals
 }
 
 const ListOFMeals = (({ name, date }) => {
