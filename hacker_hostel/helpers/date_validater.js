d83bb6ca72b6d4e73b4b9b7dc5e73c742c7a7679
Dima Kurbatov <dskurbatov13@gmail.com>
2018-11-14 19:14:08 -0800

    modefied date_validater file

--- hacker_hostel/helpers/date_validater.js
+++ hacker_hostel/helpers/date_validater.js
@@ -1,50 +1,29 @@
-function dateFormate(array){
+function isDateValid(array){
 	const currentYear = new Date().getFullYear()
 	if(array.length !== 3){
 		return false
 	}
+
+	const isEvery = array.every(item => !isNaN(item))
+	if(!isEvery) {
+		return false
+	}
+	
 	const [year, month, date] = array
-	if(isNaN(year) || year.length !== 4 || +year < currentYear){
+	if(year.length !== 4 || +year < currentYear){
 		return false
 	}
 	
-	if(isNaN(month) || month.length !== 2 || +month > 12){
+	if(month.length !== 2 || +month > 12){
 		return false
 	}
 	
-	if(isNaN(date) || date.length !== 2 || +date > 31){
+	if(date.length !== 2 || +date > 31){
 		return false
 	}
 	return true
 }
 
-function isValidDateAndRange(str1, str2){
-	const d1 = str1.split('-')
-	const d2 = str2.split('-')
-	if(dateFormate(d1) && dateFormate(d2)){
-		const from = new Date(...d1)
-		const to = new Date(...d2)
-		return to.getTime() > from.getTime()
-	}
-	return false
-}
+export default isDateValid
 
-//export default isValidDateAndRange
-function assert(number, actual, expected, testName){
-	if(actual === expected){
-		console.log(number, 'passed')
-	} else {
-		console.log(number, `FAILED [${testName}] Expected "${expected}" to be equal "${actual}"`)
-	}
-}
 
-//tests
-assert(1, false, dateFormate(["2018","to", "12"]), 'should return false if date string contains not a number')
-assert(2, false, dateFormate(["2017","11","12"]), 'should return false if year is less than current year')
-assert(3, false, dateFormate(["2018","9", "12"]), 'should return false if month is present as one digit')
-assert(4, false, dateFormate(["2018", "9", "12"]), 'should return false if month is present as one digit')
-assert(5, false, dateFormate(["2018", "09", "2"]), 'should return false if date is present as one digit')
-assert(6, false, dateFormate(["2018","13", "02"]), 'should return false if month is greater than 12')
-assert(7, false, dateFormate(["2018", "12", "32"]), 'should return false if date is greater than 31')
-assert(8, true, dateFormate(["2018", "12", "31"]), 'should return true')
-assert(9, false, dateFormate(["2018", "12"]), 'should return false if month or date or year is not present')
