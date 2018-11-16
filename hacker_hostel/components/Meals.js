28bee2bd1fefc0c52223306129bb08e784eb61f0
Dima Kurbatov <dskurbatov13@gmail.com>
2018-11-15 22:45:18 -0800

    rewrote component to except calendar as prop and render breakfast, lunch and dinner by date

--- hacker_hostel/components/Meals.js
+++ hacker_hostel/components/Meals.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react'
 import {PropTypes} from 'prop-types'
 
-const Meals = (({name, date}) => {
+const Meals = (({ calendar }) => {
+  //const dates = Object.keys(calendar).sort(sortAlphabet)
+  const sortedDates = Object.keys(calendar)
+  const result = []
+  let date = null
+  for(let i = 0, len = sortedDates.length; i < len; i++){
+    date = sortedDates[i]
+    result.push(calendar[date].map(name => <li className="morning">{`Breakfast for ${name} on ${date}`}</li>))
+    result.push(calendar[date].map(name => <li className="afternoon">{`Lunch for ${name} on ${date}`}</li>))
+    result.push(calendar[date].map(name => <li className="night">{`Dinner for ${name} on ${date}`}</li>))
+  }
   return(
     <div>
-			<li className="morning">{`Breakfast for ${name} on ${date}`}</li>
-			<li className="afternoon">{`Lunch for ${name} on ${date}`}</li>
-			<li className="night">{`Dinner for ${name} on ${date}`}</li>
+      {result}
 		</div>
   )
 })
 
+//<li className="morning">{`Breakfast for ${name} on ${date}`}</li>
+//<li className="afternoon">{`Lunch for ${name} on ${date}`}</li>
+//<li className="night">{`Dinner for ${name} on ${date}`}</li>
 Meals.propTypes = {
-  name: PropTypes.string,
-  date: PropTypes.string
+  calendar: PropTypes.object
 }
 
 export default Meals
