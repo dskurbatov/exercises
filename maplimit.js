6f990a305f305e571d0c63509a333903f6d62751
Dima Kurbatov <dskurbatov13@gmail.com>
2018-12-14 22:48:06 -0800

    added gitignore, package.json and maplimit files

---
+++ maplimit.js
@@ -0,0 +1,60 @@
+/*
+  maplimit(collection, concurancyLevel, asyncfunc, callback)
+  asyncfunc(item) => item apply to each element of the collections
+  callback(err, collection) => collection or err 
+*/
+
+
+function mapLimit(collection, cl, asyncFunc, done){
+  const len = collection.length
+  const arr = Array(len).fill(null)
+  let i = 0
+  let currentNum = 0;
+
+  (function () {
+    return new Promise((resolve, reject) => {
+      function help(){
+        while(i < len && currentNum <= cl) {
+          currentNum++
+          asyncFunc(collection[i])
+            .then((function (idx){
+              return (item) => {
+                arr[idx] = item
+                currentNum--
+                help()
+            }})(i++))
+            .catch((err) => reject(err))
+        }
+        if(currentNum === 0){
+          resolve()
+        }
+      }
+      help()
+    })
+  })()
+  .then(() => done(null, arr))
+  .catch((err) => done(err))
+}
+
+function powerOf2(x){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(typeof x === 'number'){
+        resolve(x**2)
+      } else {
+        reject('Not a number')
+      }
+    }, Math.random()*1000) 
+  })
+}
+
+function log(idx){
+  return (err, arr) => {
+    console.log(idx, err, arr)
+  }
+}
+
+mapLimit([], 2, powerOf2, log(1))
+mapLimit([1,2,"a",4,5], 2, powerOf2, log(2))
+mapLimit(["a",2,3,4,5], 2, powerOf2, log(3))
+mapLimit([1,2,3,4,5], 2, powerOf2, log(4))
